a481b01497c8b9253cfcfdc00c96be6b381ec0d0
Pierre Njiné Happy <pierrejeremieryan.njinehappy@student.vives.be>
2025-02-09 23:39:16 +0100

    Initial commit

---
+++ src/api/study-blog/controllers/study-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * study-blog controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::study-blog.study-blog');
---
+++ src/api/study-blog/routes/study-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * study-blog router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::study-blog.study-blog');
