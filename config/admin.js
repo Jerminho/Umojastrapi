01e4d7d8b1ab52f589113777a46b3c94b9d2be06
Pierre Njiné Happy <pierrejeremieryan.njinehappy@student.vives.be>
2025-02-10 07:06:50 +0100

    strapi typescript

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env("ADMIN_JWT_SECRET"),
+    secret: env("ADMIN_JWT_SECRET", "o8u6VHnutsqdzHbeKPLZHw=="),
   },
   apiToken: {
-    salt: env("API_TOKEN_SALT"),
+    salt: env("API_TOKEN_SALT", "m5+it7wqik0imWyPjX11lg=="),
   },
   transfer: {
     token: {
-      salt: env("TRANSFER_TOKEN_SALT"),
+      salt: env("TRANSFER_TOKEN_SALT", "G6Bd/SNQ9d3NZqSszd06fw=="),
     },
   },
   flags: {
