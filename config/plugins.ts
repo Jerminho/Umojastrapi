db7e1895bef6f8b1fffe3d7cdaf833fb79005b1f
Pierre Njiné Happy <pierrejeremieryan.njinehappy@student.vives.be>
2025-02-11 12:49:52 +0100

    cloudinary config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default ({ env }: { env: (key: string) => string }) => ({
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
+      },
+    },
+  });
